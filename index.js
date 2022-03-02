262594332cddd3a3905275c9727ad67801d5fc74
Mason Stooksbury <40926080+MasonStooksbury@users.noreply.github.com>
2022-03-02 11:25:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-//this is a test
+// another test
 
 const TOKEN = process.env.TOKEN;
 
